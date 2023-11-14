9a27c37ebb02caa110c8025349f16e2ed309fbc6
Shlok Agrawal <46015054+agrawalshlok@users.noreply.github.com>
2023-11-14 13:34:28 +0530

    Update fenwickTree.cpp

--- fenwickTree.cpp
+++ fenwickTree.cpp
@@ -1,21 +1,23 @@
+// jiangly 
 template <typename T>
-struct Fenwick {
+struct Fenwick {  // 1-based
     const int n;
     std::vector<T> a;
-    Fenwick(int n) : n(n), a(n) {}
-    void add(int x, T v) {
+    Fenwick(int m) : n(m), a(m) {}
+    void add (int x, T v) { // add v to index x + 1
         for (int i = x + 1; i <= n; i += i & -i) {
             a[i - 1] += v;
         }
     }
-    T sum(int x) {
+    T sum (int x) {  // sum till index x (1...x)
         T ans = 0;
         for (int i = x; i > 0; i -= i & -i) {
             ans += a[i - 1];
         }
         return ans;
     }
-    T rangeSum(int l, int r) {
+
+    T rangeSum (int l, int r) {
         return sum(r) - sum(l);
     }
 };
